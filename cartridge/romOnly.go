92e11eb1a534380f818f0e7cfd6785f6e12a8dcd
deybis <damv2007@gmail.com>
2025-05-19 11:26:58 -0600

    modificado fatal a print para que pueda ejecutar sin detener

--- cartridge/romOnly.go
+++ cartridge/romOnly.go
@@ -19,5 +19,5 @@ func (r *romOnly) Read(addr uint16) byte {
 
 func (r *romOnly) Write(addr uint16, value byte) {
 	// romOnly no permite escritura
-	log.Fatalf("Intento de escritura en ROM en %04X: %02X\n", addr, value)
+	log.Printf("Intento de escritura en ROM en %04X: %02X\n", addr, value)
 }
