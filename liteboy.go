cf208cc9c6a00d3ead732bc6cb13b34accfa0168
deybis <damv2007@gmail.com>
2025-05-19 11:05:05 -0600

    separado liteboy de main y ajustado a 60fps

---
+++ liteboy.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/deybismelendez/liteboy/bus"
+	"github.com/deybismelendez/liteboy/cpu"
+	"github.com/deybismelendez/liteboy/ppu"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+const (
+	ScreenWidth  = 160
+	ScreenHeight = 144
+	Scale        = 4
+)
+
+type Liteboy struct {
+	cpu         *cpu.CPU
+	ppu         *ppu.PPU
+	bus         *bus.Bus
+	cycles      int
+	targetTPS   int
+	tpsMode     []int
+	fastForward int
+	image       *ebiten.Image
+}
+
+func NewLiteboy(cpu *cpu.CPU, ppu *ppu.PPU, bus *bus.Bus) *Liteboy {
+	return &Liteboy{
+		cpu:         cpu,
+		ppu:         ppu,
+		bus:         bus,
+		image:       ebiten.NewImage(ScreenWidth, ScreenHeight),
+		tpsMode:     []int{70224, 70224 * 2, 70224 * 3, 70224 * 4},
+		fastForward: 1,
+	}
+}
+
+func (liteboy *Liteboy) Update() error {
+	// Ejecutamos ciclos hasta llegar al target para refrescar la pantalla
+
+	for liteboy.cycles < liteboy.tpsMode[liteboy.targetTPS] {
+		liteboy.cycles += liteboy.cpu.Step()
+		liteboy.handleGamepad()
+
+	}
+	liteboy.handleKeyboard()
+	liteboy.image.WritePixels(liteboy.ppu.Framebuffer)
+	liteboy.cycles -= liteboy.tpsMode[liteboy.targetTPS]
+
+	return nil
+}
+
+func (liteboy *Liteboy) Draw(screen *ebiten.Image) {
+	// Escalar la imagen a la ventana (multiplicando el tamaño)
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(Scale, Scale)
+	screen.DrawImage(liteboy.image, op)
+
+	// Mostrar FPS en pantalla
+	msg := fmt.Sprintf("LiteBoy Emulator - Press ESC to quit\nFPS: %.2f TPS: %.2f Target TPS: %d", ebiten.ActualFPS(), ebiten.ActualTPS()*float64(liteboy.tpsMode[liteboy.targetTPS])/60, liteboy.tpsMode[liteboy.targetTPS])
+	ebitenutil.DebugPrint(screen, msg)
+}
+
+func (liteboy *Liteboy) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return ScreenWidth * Scale, ScreenHeight * Scale
+}
+
+func (liteboy *Liteboy) handleKeyboard() {
+	if ebiten.IsKeyPressed(ebiten.KeySpace) {
+		liteboy.targetTPS = liteboy.fastForward
+	} else {
+		liteboy.targetTPS = 0
+	}
+}
+
+func (liteboy *Liteboy) handleGamepad() {
+	// Leer el valor del registro P1 (0xFF00)
+	p1 := liteboy.bus.Read(0xFF00)
+
+	// Bit 4: dirección (0=activado), Bit 5: botones
+	directionKeys := (p1 & (1 << 4)) == 0
+	buttonKeys := (p1 & (1 << 5)) == 0
+
+	var input byte = 0x0F // bits 0-3: todos presionados (1 = no presionado)
+
+	if directionKeys {
+		if ebiten.IsKeyPressed(ebiten.KeyRight) {
+			input &= ^byte(1 << 0) // Bit 0 - Derecha
+		}
+		if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+			input &= ^byte(1 << 1) // Bit 1 - Izquierda
+		}
+		if ebiten.IsKeyPressed(ebiten.KeyUp) {
+			input &= ^byte(1 << 2) // Bit 2 - Arriba
+		}
+		if ebiten.IsKeyPressed(ebiten.KeyDown) {
+			input &= ^byte(1 << 3) // Bit 3 - Abajo
+		}
+	}
+
+	if buttonKeys {
+		if ebiten.IsKeyPressed(ebiten.KeyZ) {
+			input &= ^byte(1 << 0) // Bit 0 - A
+		}
+		if ebiten.IsKeyPressed(ebiten.KeyX) {
+			input &= ^byte(1 << 1) // Bit 1 - B
+		}
+		if ebiten.IsKeyPressed(ebiten.KeySpace) {
+			input &= ^byte(1 << 2) // Bit 2 - Select
+		}
+		if ebiten.IsKeyPressed(ebiten.KeyEnter) {
+			input &= ^byte(1 << 3) // Bit 3 - Start
+		}
+	}
+
+	// Escribir bits 0-3 en el registro FF00 sin tocar bits 4-7
+	liteboy.bus.Write(0xFF00, (p1&0xF0)|input)
+}
--- main.go
+++ main.go
@@ -12,117 +12,7 @@ import (
 	"github.com/deybismelendez/liteboy/timer"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
-
-const (
-	ScreenWidth  = 160
-	ScreenHeight = 144
-	Scale        = 4
-	TargetCycle  = 70224
-)
-
-type Game struct {
-	cpu       *cpu.CPU
-	ppu       *ppu.PPU
-	bus       *bus.Bus
-	lastCycle int
-	image     *ebiten.Image
-}
-
-func NewGame(cpu *cpu.CPU, ppu *ppu.PPU, bus *bus.Bus) *Game {
-	return &Game{
-		cpu:   cpu,
-		ppu:   ppu,
-		bus:   bus,
-		image: ebiten.NewImage(ScreenWidth, ScreenHeight),
-	}
-}
-
-func (g *Game) Update() error {
-	// Ejecutamos ciclos hasta llegar al target para refrescar la pantalla (simil a frame)
-	g.lastCycle += g.cpu.Step()
-	if g.lastCycle > TargetCycle {
-		g.lastCycle -= TargetCycle
-		g.image.WritePixels(g.ppu.Framebuffer)
-	}
-	// Leer el valor del registro P1 (0xFF00)
-	p1 := g.bus.Read(0xFF00)
-
-	// Bit 4: dirección (0=activado), Bit 5: botones
-	directionKeys := (p1 & (1 << 4)) == 0
-	buttonKeys := (p1 & (1 << 5)) == 0
-
-	var input byte = 0x0F // bits 0-3: todos presionados (1 = no presionado)
-
-	if directionKeys {
-		if ebiten.IsKeyPressed(ebiten.KeyRight) {
-			input &= ^byte(1 << 0) // Bit 0 - Derecha
-		}
-		if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-			input &= ^byte(1 << 1) // Bit 1 - Izquierda
-		}
-		if ebiten.IsKeyPressed(ebiten.KeyUp) {
-			input &= ^byte(1 << 2) // Bit 2 - Arriba
-		}
-		if ebiten.IsKeyPressed(ebiten.KeyDown) {
-			input &= ^byte(1 << 3) // Bit 3 - Abajo
-		}
-	}
-
-	if buttonKeys {
-		if ebiten.IsKeyPressed(ebiten.KeyZ) {
-			input &= ^byte(1 << 0) // Bit 0 - A
-		}
-		if ebiten.IsKeyPressed(ebiten.KeyX) {
-			input &= ^byte(1 << 1) // Bit 1 - B
-		}
-		if ebiten.IsKeyPressed(ebiten.KeySpace) {
-			input &= ^byte(1 << 2) // Bit 2 - Select
-		}
-		if ebiten.IsKeyPressed(ebiten.KeyEnter) {
-			input &= ^byte(1 << 3) // Bit 3 - Start
-		}
-	}
-
-	// Escribir bits 0-3 en el registro FF00 sin tocar bits 4-7
-	g.bus.Write(0xFF00, (p1&0xF0)|input)
-	// Actualizar la imagen con el framebuffer RGBA (suponiendo que ppu.Framebuffer es []byte RGBA8888)
-	// ebiten espera un slice []byte con pixels en formato RGBA8888
-
-	// Asegurarse que el framebuffer tenga el tamaño correcto
-	if len(g.ppu.Framebuffer) != ScreenWidth*ScreenHeight*4 {
-		return fmt.Errorf("framebuffer size incorrecta")
-	}
-
-	// Copy pixels directo a ebiten.Image
-	// ebiten.Image.WritePixels espera []byte en formato RGBA8888
-
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	// Escalar la imagen a la ventana (multiplicando el tamaño)
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Scale(Scale, Scale)
-	screen.DrawImage(g.image, op)
-
-	// Mostrar FPS en pantalla
-	msg := fmt.Sprintf("LiteBoy Emulator - Press ESC to quit\nFPS: %.2f", ebiten.ActualFPS())
-	ebitenutil.DebugPrint(screen, msg)
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return ScreenWidth * Scale, ScreenHeight * Scale
-}
-
-func loadROM(path string) *cartridge.Cartridge {
-	cart := cartridge.NewCartridge(path)
-	if cart == nil {
-		log.Fatalf("Error al cargar ROM: %s", path)
-	}
-	return cart
-}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -132,23 +22,23 @@ func main() {
 
 	romPath := os.Args[1]
 	if len(os.Args) == 3 && os.Args[2] == "--info" {
-		cart := loadROM(romPath)
+		cart := cartridge.NewCartridge(romPath)
 		cart.PrintHeaderInfo()
 		os.Exit(0)
 	}
 
-	cart := loadROM(romPath)
+	cart := cartridge.NewCartridge(romPath)
 	gameBus := bus.NewBus(cart)
 	gamePPU := ppu.NewPPU(gameBus)
 	gameTimer := timer.NewTimer(gameBus)
 	gameCPU := cpu.NewCPU(gameBus, gameTimer, gamePPU)
 
-	game := NewGame(gameCPU, gamePPU, gameBus)
+	game := NewLiteboy(gameCPU, gamePPU, gameBus)
 
 	// Configurar ventana y correr el loop de Ebiten
 	ebiten.SetWindowSize(ScreenWidth*Scale, ScreenHeight*Scale)
 	ebiten.SetWindowTitle("LiteBoy Emulator")
-	ebiten.SetTPS(800_000)
+	ebiten.SetTPS(60)
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
